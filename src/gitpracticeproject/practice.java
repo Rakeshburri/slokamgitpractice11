cb8985968dc349b3f7447fc9528e78f10386d1c6
Rakesh <rakesh.burri2000.com>
2023-06-06 12:42:01 +0530

    first commit

---
+++ src/gitpracticeproject/practice.java
@@ -0,0 +1,17 @@
+package gitpracticeproject;
+
+public class practice {
+	public int a =15;
+	public int b =63;
+	public int c = a+b;
+	
+	public static void main(String[] args) {
+		practice p = new practice();
+	
+		System.out.println(p.a);
+		System.out.println(p.b);
+		System.out.println(p.c);
+	}
+	
+
+}
